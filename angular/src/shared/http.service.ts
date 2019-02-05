0b7d6e0a70f01570c6a3bbc15a98673d6e636338
Denis Pupyrev <adaptmen@gmail.com>
2019-02-06 03:46:59 +0500

    dev

--- angular/src/shared/http.service.ts
+++ angular/src/shared/http.service.ts
@@ -7,10 +7,12 @@ export class HttpService {
 	constructor(private http: HttpClient) { }
 
 	public get(url, options?) {
+		console.log('==>', url);
 		return this.http.get(url, options || {});
 	}
 
 	public post(url, body, options?) {
+		console.log('<==');
 		return this.http.post(url, body, options || {});
 	}
 

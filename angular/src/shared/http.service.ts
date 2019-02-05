258f27baedd956faf38e088d50088caaf0e4d3c6
Denis Pupyrev <adaptmen@gmail.com>
2019-02-06 03:52:53 +0500

    dev

--- angular/src/shared/http.service.ts
+++ angular/src/shared/http.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import { Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class HttpService {
 	constructor(private http: HttpClient) { }
 
 	public get(url, options?) {
-		console.log('==>', url);
-		return this.http.get(url, options || {});
+		console.log('get==>', url);
+		let subj = new Subject();
+		this.http
+		.get(url, options || {})
+		.subscribe((data) => {
+			console.log('<==get', url, data);
+			subj.next(data);
+		});
+		return subj;
 	}
 
 	public post(url, body, options?) {
-		console.log('<==', url, body);
-		return this.http.post(url, body, options || {});
+		console.log('post==>', url, body);
+		let subj = new Subject();
+		this.http
+		.post(url, body, options || {})
+		.subscribe((data) => {
+			console.log('<==post', url, data);
+			subj.next(data);
+		});
+		return subj;
 	}
 
 }
